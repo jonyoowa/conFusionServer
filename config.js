f62a6264d89cabf9f817ab3eda8af80f70afc27e
Jon Yoo <jonyoowa@outlook.com>
2019-09-05 12:04:42 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321', // Secret key for signing the JSON Web Token 
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion' // Specify mongo URL, the url for mongoDB server
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion', // Specify mongo URL, the url for mongoDB server
+    'facebook': {
+        clientId: '548615642344598',
+        clientSecret: 'e4e0c9a2dda070bb75a004679d09c021' 
+    }
 }
